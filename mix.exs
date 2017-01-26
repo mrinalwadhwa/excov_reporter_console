758fc0716c425f7de44a9783b755b507dfe5830b
Mrinal Wadhwa <mwadhwa@fybr-tech.com>
2017-01-26 20:12:49 +0530

    make excov a hex deps and bump version to v0.1.6

--- mix.exs
+++ mix.exs
@@ -3,14 +3,14 @@ defmodule ExCov.Reporter.Console.Mixfile do
 
   def project do
     [app: :excov_reporter_console,
-     version: "0.1.5",
+     version: "0.1.6",
      description: "Console Reporter for ExCov",
      deps: deps(),
      package: package()]
   end
 
   defp deps do
-    [{:excov, "0.1.7", github: "mrinalwadhwa/excov"}]
+    [{:excov, "0.1.7"}]
   end
 
   defp package do
