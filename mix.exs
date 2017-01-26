51fb749338bfe9ef2d8568a836c2a46bf6bea5fe
Mrinal Wadhwa <mwadhwa@fybr-tech.com>
2017-01-26 20:18:54 +0530

    bump version v0.1.7

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCov.Reporter.Console.Mixfile do
 
   def project do
     [app: :excov_reporter_console,
-     version: "0.1.6",
+     version: "0.1.7",
      description: "Console Reporter for ExCov",
      deps: deps(),
      package: package()]
